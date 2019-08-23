af99a3d06713e4b497cc7ddbcad6cc3fed98eb44
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-23 09:09:59 -0700

    table set for pets

--- db/migrate/20190823154801_add_pets_table.rb
+++ db/migrate/20190823154801_add_pets_table.rb
@@ -1,6 +1,9 @@
 class AddPetsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :pets do |t|
-      t.column :
+      t.column :type, :string
+      t.column :name, :string
+      t.column :age, :integer
+      t.column :gender, :string
   end
 end
